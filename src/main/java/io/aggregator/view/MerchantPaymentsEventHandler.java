48ee958ecde7d2b75ee56976acf491c1c1a32b39
mckeeh3 <mckeeh3@gmail.com>
2022-03-06 05:32:08 -0500

    fix merchant-payment query

--- src/main/java/io/aggregator/view/MerchantPaymentsEventHandler.java
+++ src/main/java/io/aggregator/view/MerchantPaymentsEventHandler.java
@@ -1,7 +1,6 @@
 package io.aggregator.view;
 
 import io.aggregator.entity.PaymentEntity;
-import io.aggregator.entity.TransactionMerchantKey;
 import io.aggregator.view.MerchantPaymentsModel.MerchantPayment;
 
 class MerchantPaymentsEventHandler {
@@ -9,14 +8,10 @@ class MerchantPaymentsEventHandler {
   static MerchantPayment handle(MerchantPayment state, PaymentEntity.PaymentAggregated event) {
     return state
         .toBuilder()
-        .setMerchantKey(
-            TransactionMerchantKey.MerchantKey
-                .newBuilder()
-                .setMerchantId(event.getMerchantKey().getMerchantId())
-                .setServiceCode(event.getMerchantKey().getServiceCode())
-                .setAccountFrom(event.getMerchantKey().getAccountFrom())
-                .setAccountTo(event.getMerchantKey().getAccountTo())
-                .build())
+        .setMerchantId(event.getMerchantKey().getMerchantId())
+        .setServiceCode(event.getMerchantKey().getServiceCode())
+        .setAccountFrom(event.getMerchantKey().getAccountFrom())
+        .setAccountTo(event.getMerchantKey().getAccountTo())
         .setPaymentId(event.getPaymentId())
         .setTransactionTotalAmount(event.getTransactionTotalAmount())
         .setTransactionCount(event.getTransactionCount())
